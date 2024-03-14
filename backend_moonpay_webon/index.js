f78715f35d2a78c030486c81c7486d1e16b46cd2
dev2-nomo <dev2@nomo.app>
2024-03-14 17:56:57 +0100

    bump version

--- backend_moonpay_webon/index.js
+++ backend_moonpay_webon/index.js
@@ -1,16 +1,12 @@
 import { MoonPay } from '@moonpay/moonpay-node';
-import express from 'express'
-import dotenv from 'dotenv'
-
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
-
 const apikey = process.env.SK_LIVE || "test";
 const moonPay = new MoonPay(apikey);
 const app = express();
-
 app.get('/moonpay_signature', (req, res) => {
     const { url } = req.query;
-
     console.log(req.query);
     if (typeof url !== 'string') {
         res.status(400).send('Invalid url');
@@ -19,10 +15,5 @@ app.get('/moonpay_signature', (req, res) => {
     const signature = moonPay.url.generateSignature(url);
     res.send({ signature });
 });
-
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () =>
-    console.log('Listening on port ' + port + "!"),
-);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on port ' + port + "!"));
